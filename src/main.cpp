ed9a56b80cdf8f748a0b17efb4897ba3b6cf134c
JayWai99 <jaydencwk@gmail.com>
2024-05-03 19:41:41 +1000

    added comments to explain the differences between pull up and pull down resistor

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,27 @@ bool state;
 void setup(void){
   usart_init(BAUD_RATE);
 
+  // It does not matter if we are implementing a pull up or
+  // pull down resistor to design a working button here, we 
+  // have to clear the data direction bit for the button to
+  // set it as an input. That's it.
   BIT_CLEAR(DDRB, PIN_BUTTON);
   BIT_SET(DDRD, PIN_LED);
 
+  // If we are using a pull up here, our job will be done here
+  // at this point by enabling the corresponding port bit for 
+  // the button as the line below is doing.
   BIT_SET(PORTB, PIN_BUTTON);
   BIT_SET(PORTD, PIN_LED);
 }
+
+// For pull down resistors, we do the same as above in the
+// code, but we do need to fiddle with our circuit a little
+// bit.
+
+// We need to supply 5V to one side of the button. On the other
+// side, we need to connect the resistor to the ground, and 
+// connect the same point to the input pin set for the button.
 
 bool button_pressed(void){
   return BIT_READ(PINB, PIN_BUTTON) == 0;
@@ -32,8 +47,8 @@ bool button_debounced(void){
     if (button_pressed()){
       return button_pressed();
     }
-    return false;
   }
+  return false;
 }
 
 int main(void){
@@ -42,10 +57,12 @@ int main(void){
   while (true){
     if (button_debounced())
     {
-        BIT_SET(PORTD, PIN_LED);
-        //BIT_FLIP(PORTD, PIN_LED);
+        //BIT_SET(PORTD, PIN_LED);
+        BIT_FLIP(PORTD, PIN_LED);
+        state = !state;
+        usart_teleplot("button state", (double) state);
     }
-    else BIT_CLEAR(PORTD, PIN_LED);
+    //else BIT_CLEAR(PORTD, PIN_LED);
   }
   
 
